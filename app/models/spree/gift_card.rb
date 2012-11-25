1c6902538bf7846289e1e835d3ee7aabd9d20de2
Ben <benbruscella@gmail.com>
2012-11-25 18:10:21 +1100

    smaller code trial

--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -62,7 +62,7 @@ module Spree
 
     def generate_code
       until self.code.present? && self.class.where(code: self.code).count == 0
-        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)
+        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)[1..8]
       end
     end
 
